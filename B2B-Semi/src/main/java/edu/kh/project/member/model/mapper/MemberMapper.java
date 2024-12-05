5bd9fc923b70b7eddd5e8c302d86dac2fa0f3e87
Jung-Dae_seung <eotmd1213@naver.com>
2024-12-05 14:48:01 +0900

    ID/PW 찾기 추가

--- B2B-Semi/src/main/java/edu/kh/project/member/model/mapper/MemberMapper.java
+++ B2B-Semi/src/main/java/edu/kh/project/member/model/mapper/MemberMapper.java
@@ -1,5 +1,7 @@
 package edu.kh.project.member.model.mapper;
 
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import edu.kh.project.member.model.dto.Member;
@@ -12,6 +14,12 @@ public interface MemberMapper {
 	 * @return
 	 */
 	Member login(String memberId);
+	
+	/** 이메일 중복 체크
+	 * @param memberEmail
+	 * @return
+	 */
+	int checkEmail(String memberEmail);
 
 	/** 아이디 중복 체크
 	 * @param memberId
@@ -30,5 +38,23 @@ public interface MemberMapper {
 	 * @return
 	 */
 	int signup(Member inputMember);
+
+	/** 아이디 찾기
+	 * @param map 
+	 * @return
+	 */
+	String srchId(Map<String, String> map);
+
+	/** 비밀번호 찾기전 해당 회원 존재 여부 조회
+	 * @param map
+	 * @return
+	 */
+	int searchUser(Map<String, String> map);
+
+	/** 비밀번호 변경
+	 * @param paramMap
+	 * @return
+	 */
+	int updatePw(Map<String, Object> paramMap);
 
 }
